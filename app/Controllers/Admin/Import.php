0cc6a9ffb295e10e9c46e2beaf13d3a2b6bdda92
STADAAG\tran.daoly <lytranuit@gmail.com>
2023-06-29 14:04:45 +0700

    upload

--- app/Controllers/Admin/Import.php
+++ app/Controllers/Admin/Import.php
@@ -1059,4 +1059,206 @@ class Import extends BaseController
             }
         }
     }
+    public function tramqcb230623()
+    {
+        // $db = db_connect();
+        // die();
+        //Đường dẫn file
+        $file = APPPATH . '../assets/up/TONG THE_23.09.22.xlsx';
+
+        /** Load $inputFileName to a Spreadsheet Object  **/
+        // $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+        // print_r($spreadsheet);
+        // die();
+        //Tiến hành xác thực file
+        $objFile = \PhpOffice\PhpSpreadsheet\IOFactory::identify($file);
+        $objData = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($objFile);
+
+        //Chỉ đọc dữ liệu
+        // $objData->setReadDataOnly(true);
+        // Load dữ liệu sang dạng đối tượng
+        $objPHPExcel = $objData->load($file);
+        //Lấy ra số trang sử dụng phương thức getSheetCount();
+        // Lấy Ra tên trang sử dụng getSheetNames();
+        //Chọn trang cần truy xuất
+        // $sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+        // //Lấy ra số dòng cuối cùng
+        // $Totalrow = $sheet->getHighestRow();
+        // //Lấy ra tên cột cuối cùng
+        // $LastColumn = $sheet->getHighestColumn();
+        // //Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+        // $TotalCol = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($LastColumn);
+
+        //Tạo mảng chứa dữ liệu
+        $data = [];
+
+        $count_sheet = $objPHPExcel->getSheetCount();
+        // print_r($count_sheet);
+        // die();
+        for ($k = 0; $k < $count_sheet; $k++) {
+
+            $sheet = $objPHPExcel->setActiveSheetIndex($k);
+            $sheet_name = $sheet->getTitle();
+            // if (strpos($sheet_name, "#") == false) continue;
+            // print_r($sheet_name);die();
+            //Lấy ra số dòng cuối cùng
+            $Totalrow = $sheet->getHighestRow();
+            //Lấy ra tên cột cuối cùng
+            $LastColumn = $sheet->getHighestColumn();
+            //Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+            $TotalCol = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($LastColumn);
+
+            //Tạo mảng chứa dữ liệu
+            $data = [];
+
+            //Tiến hành lặp qua từng ô dữ liệu
+            //----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+            $row = 7;
+            for ($i = $row; $i <= $Totalrow; $i++) {
+                //----Lặp cột
+                for ($j = 0; $j < $TotalCol; $j++) {
+                    // Tiến hành lấy giá trị của từng ô đổ vào mảng
+                    $cell = $sheet->getCellByColumnAndRow($j, $i);
+
+                    $data[$i -  $row][$j] = $cell->getValue();
+                    ///CHUYEN RICH TEXT
+                    if ($data[$i -  $row][$j] instanceof \PhpOffice\PhpSpreadsheet\RichText\RichText) {
+                        $data[$i -  $row][$j] = $data[$i -  $row][$j]->getPlainText();
+                    }
+
+                    ////CHUYEN DATE 
+                    // if (\PhpOffice\PhpSpreadsheet\Shared\Date::isDateTime($cell) && $data[$i - 1][$j] > 0) {
+                    //     if (is_numeric($data[$i - 1][$j])) {
+                    //         $data[$i - 1][$j] =  $cell->getFormattedValue();
+                    //     }
+                    // }
+                }
+            }
+
+
+            // echo "<pre>";
+            // echo $sheet_name . "<br>";
+            // print_r($data);
+            // die();
+
+            $SampleModel = model("SampleModel");
+            $SampleTimeModel = model("SampleTimeModel");
+            $SampleModel->where("factory_id", 2)->delete();
+            $SampleTimeModel->where("factory_id", 2)->delete();
+            $prev_name = "";
+            $prev_code_batch = "";
+            $prev_env_name = "";
+            foreach ($data as $row) {
+
+                $name = $row[3];
+                if ($name == "") continue;
+                // $row[1] = trim($row[1]);
+                // $explode =  explode(".", $row[1]);
+                // if (count($explode) < 2) continue;
+
+                // $version = $row[2];
+                $outline_number = $row[2];
+                $code_research = null;
+                $code = $row[4];
+                $code_batch = $row[5];
+                $code_analysis = null;
+                if (is_numeric($row[6])) {
+                    $date_manufacture = date("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[6]));
+                } else {
+                    $date_manufacture = NULL;
+                }
+                if (is_numeric($row[7])) {
+                    $date_storage = date("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[7]));
+                } else {
+                    $date_storage = NULL;
+                }
+                // $time_text = $row[15];
+
+                if (is_numeric($row[10])) {
+                    $date_theory = date("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[10]));
+                } else {
+                    $date_theory = NULL;
+                }
+
+                // if (is_numeric($row[12])) {
+                //     $date_reality = date("Y-m-d", \PhpOffice\PhpSpreadsheet\Shared\Date::excelToTimestamp($row[12]));
+                // } else {
+                $date_reality = NULL;
+                // }
+                $env_code = $row[12];
+                $env_name = trim($row[11]);
+                $note = $row[15];
+                $num_get = $row[14];
+                // $num_get = explode(" ", $num_get);
+                // $num_get = $num_get[0];
+                $location =  $row[13];
+                $type_id = null;
+                $time_text = "";
+                if (str_contains($env_name, 'Dài hạn (ASEAN)')) {
+                    $type_id = 3;
+                    $time_text = trim(str_replace('Dài hạn (ASEAN)', "", $env_name));
+                } elseif (str_contains($env_name, 'Dài hạn (EU)')) {
+                    $type_id = 4;
+                    $time_text = trim(str_replace('Dài hạn (EU)', "", $env_name));
+                } elseif (str_contains($env_name, 'Lão hóa')) {
+                    $type_id = 1;
+                    $time_text = trim(str_replace('Lão hóa', "", $env_name));
+                }
+                echo $env_name . " <br>";
+                echo $type_id . " <br>";
+                echo $time_text . " <br>";
+                $factory_id = 2;
+
+                if ($name != $prev_name || $code_batch != $prev_code_batch) {
+                    $prev_name = $name;
+                    $prev_code_batch = $code_batch;
+                    $prev_env_name = $env_name;
+                    $array = array(
+                        // 'other' => $explode,
+                        'code' => $code,
+                        'outline_number' => $outline_number,
+                        'code_research' => $code_research,
+                        'code_analysis' => $code_analysis,
+                        'name' => $name,
+                        'code_batch' => $code_batch,
+                        'date_manufacture' => $date_manufacture,
+                        'date_storage' => $date_storage,
+                        'factory_id' => $factory_id,
+                        // 'location' => $location
+                    );
+                    // print_r($array);
+                    $id = $SampleModel->insert($array);
+                }
+                $time_array = explode(" ", $time_text);
+                $type_time = "M";
+                $type_time_text = "months";
+                $time = $time_array[0];
+                $based = "date_storage";
+                if ($date_storage != "") {
+                    $date_test = date("Y-m-d", strtotime($date_manufacture . " +$time $type_time_text"));
+                    if ($date_test == $date_theory) {
+                        $based = "date_manufacture";
+                    }
+                }
+                $array = array(
+                    'name' => $env_code,
+                    'note' => $note,
+                    'date_theory' => $date_theory,
+                    'date_reality' => $date_reality,
+                    'based' => $based,
+                    'time' => $time,
+                    'sample_id' => $id,
+                    'type_time' => $type_time,
+                    'factory_id' => $factory_id,
+                    'type_id' => $type_id,
+                    'num_get' => $num_get,
+                    'location' => $location
+                );
+                // print_r($array);
+
+                $SampleTimeModel->insert($array);
+            }
+        }
+    }
 }
