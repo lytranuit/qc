5dafd8794f46bc608c67a843a441a36468937d90
ASTACORP\tran.dl <lytranuit@gmail.com>
2026-04-13 15:47:10 +0700

    update

--- app/Views/backend/sample/edit.php
+++ app/Views/backend/sample/edit.php
@@ -226,7 +226,7 @@
             <input type="text" name="location" class="form-control form-control-sm location" placeholder="Vị trí">
         </td>
         <td>
-            <select class="form-control form-control-sm type_id" name="type_id">
+            <select class="form-control form-control-sm type_id" name="type_id" readonly>
                 <option value="1">Lão hóa</option>
                 <option value="2">Trung gian</option>
                 <option value="3">Dài hạn (Vùng IV)</option>
